246daf4326fac6138c3986d4d09f24b4f66bcc17
mohsinghani <mohsinghani.777@gmail.com>
2024-08-07 22:59:10 +0500

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import "@/styles/globals.css";
+import "../scss/index.scss";
+import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <PrimeReactProvider>
+      <Component {...pageProps} />
+    </PrimeReactProvider>
+  );
 }
